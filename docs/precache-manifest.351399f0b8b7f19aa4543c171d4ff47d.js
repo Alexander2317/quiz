abdb1980681467ddd874563981ab99554e818125
Alexandr Popov <cahek2317@mail.ru>
2020-09-15 20:44:34 +0300

    0.0.2

---
+++ docs/precache-manifest.351399f0b8b7f19aa4543c171d4ff47d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "087fca2ad6f8b3fa614cb872c0280bad",
+    "url": "index.html"
+  },
+  {
+    "revision": "68be807ffe75574e64dc",
+    "url": "static/css/main.839d4368.chunk.css"
+  },
+  {
+    "revision": "213ef2d75814ec7f02ba",
+    "url": "static/js/2.e320b56a.chunk.js"
+  },
+  {
+    "revision": "065ba891bac8df5940a0794605944dff",
+    "url": "static/js/2.e320b56a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68be807ffe75574e64dc",
+    "url": "static/js/main.5e179b65.chunk.js"
+  },
+  {
+    "revision": "f2f448c7a5c21ae0c7e0",
+    "url": "static/js/runtime-main.abb62f1f.js"
+  }
+]);
